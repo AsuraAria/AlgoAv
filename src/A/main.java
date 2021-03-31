dfadf8d217dc61c8808d43d65beb8c8b6bded18b
AsuraAria <48623275+AsuraAria@users.noreply.github.com>
2021-03-31 16:06:05 +0200

    Many things

--- src/A/main.java
+++ src/A/main.java
@@ -1,13 +1,17 @@
 package A;
 
-import java.util.Arrays;
+import Resources.CoinList;
 
 public class main
 {
     public static void main (String[] args)
     {
-        double test[] = {2,1,0.5,0.2,0.1,0.05,0.02,0.01};
-        CoinList coinList = new CoinList("Coins values",test);
-        coinList.showCoinList();
+        CoinList A = new CoinList(new double[]{2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01});
+        //A.show();
+        A.addCoin(2);
+        A.addCoin(20);
+        A.addCoin(.2000);
+        A.show();
+
     }
 }
---
+++ src/Resources/CoinList.java
@@ -0,0 +1,70 @@
+package Resources;
+
+import java.util.Arrays;
+
+public class CoinList {
+
+    private double[] valueList; // list of coins value
+    private int nbCoin; // number of coins taken
+    private int[] coinList; // array for coins taken
+
+
+    //CONSTRUCTORS
+
+    public CoinList(double[] valueList) {
+        this.valueList = valueList;
+        this.nbCoin = 0;
+        this.coinList = new int[valueList.length];
+    }
+
+    //GETTERS
+    public int getNbCoin() {
+        return nbCoin;
+    }
+
+    public int[] getCoinList() {
+        return coinList;
+    }
+
+    public double[] getValueList() {
+        return valueList;
+    }
+
+
+    //FUNCTIONS
+
+    // Increment coin number tracker
+
+    public void incNbCoin(int value) {
+        this.coinList[value] += 1;
+        this.nbCoin += 1;
+    }
+
+    // Show how coins values, coins list, and coins total
+
+    public void show() {
+        System.out.println("Current coins value is:");
+        System.out.println(Arrays.toString(this.getValueList()));
+        System.out.println("There is " + this.getNbCoin() + " coin(s) currently :");
+        System.out.println(Arrays.toString(this.getCoinList()));
+    }
+
+    public void addCoin(double value) {
+        boolean flag = false;
+        for (int i = 0; i < valueList.length; i++)
+        {
+            double curentValue = this.valueList[i];
+            if (curentValue == value)
+            {
+                this.incNbCoin(i);
+                System.out.println("Coin \"" + value + "\" added");
+                flag = true;
+            }
+        }
+
+        if (!flag)
+        {
+            System.out.println("Coin value is incorrect");
+        }
+    }
+}
