70647d8c49116ce2b6033c84e46640f975052537
AsuraAria <48623275+AsuraAria@users.noreply.github.com>
2021-04-19 23:05:14 +0200

    Uniformisation des call

--- src/A/mainA.java
+++ src/A/mainA.java
@@ -4,9 +4,9 @@ import resources.CoinList;
 
 public class mainA {
     public static void main(String[] args) {
-        AlgoGen AG1 = new AlgoGen(new CoinList(new float[]{.01f, 1, 0.50f, .20f, .10f, .05f, .02f, 2}), 0);
-        AlgoGen AG2 = new AlgoGen(new CoinList(new float[]{13, 5, 0.50f, .22f, .1f, .05f, .02f, 2}), 22.86f);
-        AlgoGen AG3 = new AlgoGen(new CoinList(new float[]{2, 1, 0.50f, .20f, .10f, .05f, .02f, .01f}), 4.54f);
+        AlgoGen AG1 = new AlgoGen(new CoinList(new float[]{2, 1, 0.50f, .20f, .10f, .05f, .02f, .01f}), 0);
+        AlgoGen AG2 = new AlgoGen(new CoinList(new float[]{2, 1, 0.50f, .20f, .10f, .05f, .02f, .01f}), 45);
+        AlgoGen AG3 = new AlgoGen(new CoinList(new float[]{2, 1, 0.50f, .20f, .10f, .05f, .02f, .01f}), 54.54f);
 
         AG1.resolve();
         AG2.resolve();
--- src/C/mainC.java
+++ src/C/mainC.java
@@ -4,8 +4,8 @@ import resources.CoinList;
 
 public class mainC {
     public static void main(String[] args) {
-        AlgoGlouton AG1 = new AlgoGlouton(new CoinList(new float[]{.01f, 1, 0.50f, .20f, .10f, .05f, .02f, 2}), 0);
-        AlgoGlouton AG2 = new AlgoGlouton(new CoinList(new float[]{.13f, 5, 0.50f, .22f, .1f, .05f, .01f, 2}), 5.86f);
+        AlgoGlouton AG1 = new AlgoGlouton(new CoinList(new float[]{2, 1, 0.50f, .20f, .10f, .05f, .02f, .01f}), 0);
+        AlgoGlouton AG2 = new AlgoGlouton(new CoinList(new float[]{2, 1, 0.50f, .20f, .10f, .05f, .02f, .01f}), 45);
         AlgoGlouton AG3 = new AlgoGlouton(new CoinList(new float[]{2, 1, 0.50f, .20f, .10f, .05f, .02f, .01f}), 54.54f);
 
         AG1.resolve();
